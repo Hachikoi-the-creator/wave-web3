a43a771d0bd370beec3f36b2add00dfb38da3688
Hachikoi-the-creator <facka72@gmail.com>
2022-04-22 17:44:58 -0500

    I guess it pseudo works

--- Hardhat/scripts/run.js
+++ Hardhat/scripts/run.js
@@ -6,7 +6,6 @@ const main = async () => {
   await waveContract.deployed();
   console.log("Contract addy:", waveContract.address);
 
-  //  * Get Contract balance
   let contractBalance = await hre.ethers.provider.getBalance(
     waveContract.address
   );
@@ -15,11 +14,15 @@ const main = async () => {
     hre.ethers.utils.formatEther(contractBalance)
   );
 
-  //  * Send Wave
-  let waveTxn = await waveContract.wave("A message!");
+  /*
+   * Let's try two waves now
+   */
+  const waveTxn = await waveContract.wave("This is wave #1");
   await waveTxn.wait();
 
-  //  * Get Contract balance to see what happened!
+  const waveTxn2 = await waveContract.wave("This is wave #2");
+  await waveTxn2.wait();
+
   contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
   console.log(
     "Contract balance:",
